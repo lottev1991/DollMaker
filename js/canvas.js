ca0b321463cf539339a3c88a6b36d02ad3e3161b
Lotte V <61566058+lottev1991@users.noreply.github.com>
2025-06-23 00:05:02 +0200

    Revert "test"
    This reverts commit 621ffb3f0c0f9f8252c9de032ef52611e5237c76.

--- js/canvas.js
+++ js/canvas.js
@@ -223,7 +223,7 @@ categories.forEach((cat, index) => {
         option.appendChild(image);
 
         //add click listener on option.firstchild so the li isn't accidentally clicked
-        option.firstChild.addEventListener("click", (clickedOption) => {
+        option.firstChild.addEventListener("mousedown", (clickedOption) => {
           //add animation
           // option.style.transform = "scale(1.05)";
           // setTimeout(() => {
@@ -354,7 +354,7 @@ function loadDolls() {
 
     dollOption.appendChild(image);
 
-    dollOption.addEventListener("click", (clickedDoll) => {
+    dollOption.addEventListener("mousedown", (clickedDoll) => {
       //add animation
       // dollOption.style.transform = "scale(1.05)";
       // setTimeout(() => {
