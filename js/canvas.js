0a47cf77d76a6b15f9258db70b23be3c8d097a2c
Lotte V <61566058+lottev1991@users.noreply.github.com>
2025-06-23 00:19:34 +0200

    Revert "debug"
    This reverts commit 43d590d09db31b3c1c5c03e94008051287843809.

--- js/canvas.js
+++ js/canvas.js
@@ -256,7 +256,7 @@ categories.forEach((cat, index) => {
               //remove all the previous options in the ui
               var optionsArray = [...options.children];
               optionsArray.forEach((option) => {
-                option.classList.toggle("clickedOption");
+                option.classList.remove("clickedOption");
               });
 
               //remove all the previous options in the doll object because we can only select 1
