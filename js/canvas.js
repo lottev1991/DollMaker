289ab0ea629d804242404aab156496b8d9db7e0e
Lotte V <61566058+lottev1991@users.noreply.github.com>
2025-06-23 04:06:35 +0200

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -223,7 +223,7 @@ categories.forEach((cat, index) => {
         option.appendChild(image);
 
         //add click listener on option.firstchild so the li isn't accidentally clicked
-        option.firstChild.addEventListener("click", (clickedOption) => {
+        option.firstChild.addEventListener("pointerdown", (clickedOption) => {
           //add animation
           option.style.transform = "scale(1.05)";
           setTimeout(() => {
