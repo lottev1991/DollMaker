0b24980ffaefc058ced1d41bd59116de65bfba8b
Lotte V <61566058+lottev1991@users.noreply.github.com>
2025-06-23 02:40:30 +0200

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -245,7 +245,7 @@ categories.forEach((cat, index) => {
             //If toggleMultiselect is false we first remove all the other options and then add the new option
             if ( toggleMultiselect ) {
               //if ( clickedOption.target.parentNode.classList.contains( "clickedOption") ) {
-                clickedOption.target.parentNode.classList.remove("clickedOption");
+                clickedOption.target.parentNode.classList.remove("clickedOption2");
               // } else {
               //   clickedOption.target.parentNode.classList.add("clickedOption");
               //}
@@ -274,7 +274,7 @@ categories.forEach((cat, index) => {
 
               //add the new clicked option in the ui
               //if ( clickedOption.target.parentNode.classList.contains( "clickedOption" ) ) {
-                clickedOption.target.parentNode.classList.remove( "clickedOption" );
+                clickedOption.target.parentNode.classList.remove( "clickedOption2" );
               // } else {
               //   clickedOption.target.parentNode.classList.add( "clickedOption" );
               //}
